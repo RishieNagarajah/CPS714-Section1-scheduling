018c496bd318e41a28b4f90425286c1515bfda4e
RishieNagarajah <rishie2442@gmail.com>
2025-11-14 22:50:29 -0500

    initial commit

---
+++ scheduling/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="Background">
+      <main className="main">
+        <div className="Overall">
+          <h1 className="Title">
+            FitHub Scheduling</h1>
+        </div>
+
+      </main>
+    </div>
+  );
+}
